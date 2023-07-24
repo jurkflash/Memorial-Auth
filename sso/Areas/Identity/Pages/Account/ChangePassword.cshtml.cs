938c22c48c414c0780a77ab113f136c18d4a527a
Wayne <jurkflash@hotmail.com>
2023-07-24 21:26:35 +0800

    Add change password page

---
+++ sso/Areas/Identity/Pages/Account/ChangePassword.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sso.Core;
+using static AutoMapper.Internal.ExpressionFactory;
+
+namespace Sso.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class ChangeIdentityPasswordModel : PageModel
+    {
+        private readonly UserManager<CCAIdentity> _userManager;
+
+        public ChangeIdentityPasswordModel(UserManager<CCAIdentity> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [Display(Name = "用户 Username")]
+            public string Username { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            [Display(Name = "密码 Current Password")]
+            public string CurrentPassword { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 7)]
+            [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z\\d]).{7,}$", ErrorMessage = "Minimum 7 characters<br/>at least one number<br/>at least one upper case<br/>at least one lower case<br/>at least one symbol")]
+            [DataType(DataType.Password)]
+            [Display(Name = "新密码 New Password")]
+            public string NewPassword { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "确定新密码 Confirm new password")]
+            [Compare("NewPassword", ErrorMessage = "The new password and confirmation new password do not match.")]
+            public string ConfirmNewPassword { get; set; }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = await _userManager.FindByNameAsync(Input.Username);
+            if (user == null)
+            {
+                // Don't reveal that the user does not exist
+                return RedirectToPage("./ResetPasswordConfirmation");
+            }
+
+            var result = await _userManager.ChangePasswordAsync(user, Input.CurrentPassword, Input.NewPassword);
+            if (result.Succeeded)
+            {
+                return RedirectToPage("./ResetPasswordConfirmation");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return Page();
+        }
+    }
+}
